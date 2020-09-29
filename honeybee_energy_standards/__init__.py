2e6a2e1fc1bebd288bf871e0a4a4e1c6cac480f0
Chris <chris@ladybug.tools>
2020-09-29 17:24:52 -0400

    fix(docs): Trigger release of new pacakge
    This is the only library that isn't tightly bound to the dependency tree so we need to manually trigger a new release.

--- honeybee_energy_standards/__init__.py
+++ honeybee_energy_standards/__init__.py
@@ -1,1 +1,5 @@
-"""honeybee-energy-standards library."""
+"""Honeybee-energy extension for standards, codes, and templates.
+
+Note this package contains no Python code and only contains JSON objects that
+can be consumed by the honeybee-energy package when installed.
+"""
