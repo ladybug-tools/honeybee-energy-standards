3c124d74375dcf1975d0c95149382d0bdae0df9a
Chris Mackey <chris@ladybug.tools>
2022-07-06 22:50:17 -0400

    fix(building): Improve building mixes with multipliers and exclude_floor

--- standards_update/_extract_bldg_mix.py
+++ standards_update/_extract_bldg_mix.py
@@ -3,8 +3,15 @@ import json
 
 from honeybee.model import Model
 
-ref_blgd_folder = 'C:/Users/chris/simulation/Baseline_Models/hbjson'
+ref_blgd_folder = 'G:/My Drive/LadybugTools/Baseline_Models/hbjson'
 output_file = 'honeybee_energy_standards/building_mix.json'
+
+# programs to exclude
+EX_PROGRAM =(
+    'Outpatient Hall_infil',
+    'LargeHotel Corridor2',
+    'LargeHotel Retail2'
+)
 
 building_mix = {}
 
@@ -12,34 +19,32 @@ for hbjson_file in os.listdir(ref_blgd_folder):
     # load the model to python
     bldg_name = hbjson_file.replace('.hbjson', '')
     hbjson_path = os.path.join(ref_blgd_folder, hbjson_file)
-    with open(hbjson_path) as inf:
-        model_dict = json.load(inf)
-    for room in model_dict['rooms']:
-        room['identifier'] = room['identifier'].replace('/', '').replace(':', '')
-        for face in room['faces']:
-            if 'apertures' in face:
-                for ap in face['apertures']:
-                    ap['identifier'] = ap['identifier'].replace('/', '').replace(':', '')
-            if 'doors' in face:
-                for dr in face['doors']:
-                    dr['identifier'] = dr['identifier'].replace('/', '').replace(':', '')
-    model = Model.from_dict(model_dict)
+    model = Model.from_hbjson(hbjson_path)
 
     # collect the floor areas for each of the space types
     bld_area_dict = {}
     for room in model.rooms:
-        if 'space_type' in room.user_data:
+        if 'space_type' in room.user_data and not room.exclude_floor_area:
             space_type = room.user_data['space_type']
-            try:
-                bld_area_dict[space_type] += room.floor_area
-            except KeyError:
-                bld_area_dict[space_type] = room.floor_area
+            if space_type.startswith('SmallHotel'):
+                space_type = space_type.replace('123', '').replace('4', '')
+                space_type = space_type.replace('Front', '').replace('Rear', '')
+            elif space_type.startswith('StripMall'):
+                space_type = space_type.replace('Strip mall', '').replace('type', 'Type')
+            elif space_type.startswith('MidriseApartment') or space_type.startswith('HighriseApartment'):
+                space_type = space_type.replace('_topfloor', '').replace('_NS', '').replace('_WE', '')
+
+            if space_type not in EX_PROGRAM:
+                try:
+                    bld_area_dict[space_type] += room.floor_area * room.multiplier
+                except KeyError:
+                    bld_area_dict[space_type] = room.floor_area * room.multiplier
 
     # turn the floor areas into fractional values
     total_area = sum(bld_area_dict.values())
     bld_fraction_dict = {}
     for key, value in bld_area_dict.items():
-        new_key = key.replace(bldg_name, '').strip()
+        new_key = key.replace(bldg_name, '', 1).replace('-', '').strip()
         new_key = '2013::{}::{}'.format(bldg_name, new_key)
         bld_fraction_dict[new_key] = round(value / total_area, 6)
     building_mix[bldg_name] = bld_fraction_dict
